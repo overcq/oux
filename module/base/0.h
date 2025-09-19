e97c23013c4d6ff1a054baf602c7201e35159ace
Janusz Augustyński <overcq@int.pl>
2025-09-19 19:30:21 +0200

    Poprawienie mechanizmu powiększania aktywnego obszaru stosu ‹zadań› podczas zmiany kolekcji ‹zadań›. Wstępna postać ‹modułu› “gui-cli”.

--- module/base/0.h
+++ module/base/0.h
@@ -30,11 +30,11 @@ extern void E_flow_Q_process_call_I_func(P);
 #define _sigprocmask(how,set,oldset)    { V0_( sigprocmask( (how), (set), (oldset) )); }
     #endif
     #ifdef C_pthreads
-#define _Z_tasks_table_S_edit_begin     E_flow_Q_task_I_touch_stack(0); Vr_( pthread_mutex_lock( &E_base_S->E_flow_Z_task_table_S_mutex )); _sigprocmask( SIG_BLOCK, &E_base_S->E_flow_Z_sigset_S_process_call_reply, &E_base_S->E_flow_Z_task_table_S_sigset ); U_L( E_base_S->E_flow_S_mode, Z_task_table_S_can_read )
-#define _Z_tasks_table_S_edit_end       U_F( E_base_S->E_flow_S_mode, Z_task_table_S_can_read ); _sigprocmask( SIG_SETMASK, &E_base_S->E_flow_Z_task_table_S_sigset, 0 ); Vr_( pthread_mutex_unlock( &E_base_S->E_flow_Z_task_table_S_mutex ))
+#define _Z_tasks_table_S_edit_begin     Vr_( pthread_mutex_lock( &E_base_S->E_flow_Z_task_table_S_mutex )); _sigprocmask( SIG_BLOCK, &E_base_S->E_flow_Z_sigset_S_process_call_reply, &E_base_S->E_flow_Z_task_table_S_sigset ); U_L( E_base_S->E_flow_S_mode, Z_task_table_S_can_read ); E_base_S->E_flow_Q_task_S_tmp.touched_stack = task_current->touched_stack
+#define _Z_tasks_table_S_edit_end       task_current->touched_stack = E_base_S->E_flow_Q_task_S_tmp.touched_stack; U_F( E_base_S->E_flow_S_mode, Z_task_table_S_can_read ); _sigprocmask( SIG_SETMASK, &E_base_S->E_flow_Z_task_table_S_sigset, 0 ); Vr_( pthread_mutex_unlock( &E_base_S->E_flow_Z_task_table_S_mutex ))
     #else
-#define _Z_tasks_table_S_edit_begin     E_flow_Q_task_I_touch_stack(0); _sigprocmask( SIG_BLOCK, &E_base_S->E_flow_Z_sigset_S_process_call_reply, &E_base_S->E_flow_Z_task_table_S_sigset ); U_L( E_base_S->E_flow_S_mode, Z_task_table_S_can_read )
-#define _Z_tasks_table_S_edit_end       U_F( E_base_S->E_flow_S_mode, Z_task_table_S_can_read ); _sigprocmask( SIG_SETMASK, &E_base_S->E_flow_Z_task_table_S_sigset, 0 )
+#define _Z_tasks_table_S_edit_begin     _sigprocmask( SIG_BLOCK, &E_base_S->E_flow_Z_sigset_S_process_call_reply, &E_base_S->E_flow_Z_task_table_S_sigset ); U_L( E_base_S->E_flow_S_mode, Z_task_table_S_can_read ); E_base_S->E_flow_Q_task_S_tmp.touched_stack = task_current->touched_stack
+#define _Z_tasks_table_S_edit_end       task_current->touched_stack = E_base_S->E_flow_Q_task_S_tmp.touched_stack; U_F( E_base_S->E_flow_S_mode, Z_task_table_S_can_read ); _sigprocmask( SIG_SETMASK, &E_base_S->E_flow_Z_task_table_S_sigset, 0 )
     #endif
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     #ifndef E_flow_drv_C_clock_monotonic
